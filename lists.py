dd5167037ab95927fb9dbb494d30d79e469b3208
Paul J. Meyer <pauljonmeyer@web.de>
2026-02-26 15:44:13 +0100

    practicing loops and lists

--- lists.py
+++ lists.py
@@ -1,29 +1,69 @@
-shopping = ["banana", "shampoo", "cereals", "tea", "napkins", "coffee", "chocolate"]
+# shopping = ["banana", "shampoo", "cereals", "tea", "napkins", "coffee", "chocolate"]
 
-count = 0
-for _ in shopping:
-    count += 1
+# count = 0
+# for _ in shopping:
+#     count += 1
 
-print("Items counted:", count)
+# print("Items counted:", count)
 
-print("Number of items on the list:", len(shopping))
-print ("First and last item form the shopping list:", shopping[0], shopping[-1])
+# print("Number of items on the list:", len(shopping))
+# print ("First and last item form the shopping list:", shopping[0], shopping[-1])
 
-numbers = [3, 12, 5, 8, 20, 1, 7]
-highnumbers = []
+# numbers = [3, 12, 5, 8, 20, 1, 7]
+# highnumbers = []
 
 
-for _ in numbers:
-    if _ > 5:
-        highnumbers.append(_)
+# for _ in numbers:
+#     if _ > 5:
+#         highnumbers.append(_)
 
-double = []
+# double = []
 
-for _ in numbers:
-    double.append(_ * 2)
+# for _ in numbers:
+#     double.append(_ * 2)
 
-print("\nList contains following numbers:", numbers)
-print ("Numbers > than 5:", highnumbers)
-print("List with doubled numbers:", double)
+# print("\nList contains following numbers:", numbers)
+# print ("Numbers > than 5:", highnumbers)
+# print("List with doubled numbers:", double)
 
-#Ich habe die Aufgaben 1-6 abgeschlossen.
+#Aufgabe 1:
+numbers = [1, 2, 3]
+
+numbers.extend([4, 5])
+print(numbers)
+
+#Aufgabe 2:
+letters = ["A", "C", "G", "T"]
+
+print(letters[0], letters[-1])      # -1 = letztes Element
+
+#Aufgabe 3:
+numbers = [4, 17, 2, 9, 12]
+highest_number = numbers[0]
+
+for key in numbers:
+    if key > highest_number:
+        highest_number = key
+
+print(highest_number)
+
+#Aufgabe 4:
+numbers = [3, 12, 7, 18, 5, 20]
+filtered = []   #neue Liste ist wichtig. Wenn man Elemente aus Listen entfernt, können diese in loops übersprungen werden.
+
+for key in numbers:
+    if key > 10:
+        filtered.append(key)
+# eleganter (list comprehension):
+filtered = [number for number in numbers if number > 10]
+
+print(filtered)
+
+#Aufgabe 5:
+numbers = []
+
+for i in range(0,9):
+    if i % 2 == 0:
+        numbers.append(i)
+
+print(numbers)
---
+++ loops.py
@@ -0,0 +1,36 @@
+#Aufgabe 1:
+
+for i in range(1, 6):
+    print(i)
+
+#Aufgabe 2:
+names = ["Anna", "Ben", "Clara"]
+
+for name in names:
+    print(name)
+
+#Aufgabe 3:
+numbers = [3, 7, 2, 9, 5]
+total = 0
+
+for number in numbers:
+    total += number
+
+print(total)
+
+#Aufgabe 4:
+numbers = [1,2,3,4,5,6,7,8]
+
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
+
+#Aufgabe 5:
+count = 5
+
+while count > 0:
+    print(count)
+    count -= 1
+else:
+    print("Start!")
+
